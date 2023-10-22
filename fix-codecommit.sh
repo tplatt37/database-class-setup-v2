be191859f1a70d6befad40af51640c0a4fa043f7
TR Platt <trplatt@amazon.com>
2023-10-22 15:03:28 +0000

    manual tool to make iteration faster

---
+++ fix-codecommit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# This is for dev use.
+# Clone down the database-schemas repo, update files and commit in
+# So you don't have to re-create the repo stack - which can't be updated.
+#
+
+cd ~/repo
+git clone codecommit::us-east-1://database-schemas
+
+cp ~/repo/database-class-setup-v2/schemas/* cd ~/repo/database-schemas
+cd ~/repo/database-schemas
+git add . 
+git commit -am "manual updates..."
+git push
+
+
+cd ~/repo/database-class-setup-v2
